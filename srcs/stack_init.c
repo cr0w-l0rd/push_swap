4a0dcda437628800e8c8b5d9a2d07b7f5fdb67b3
May Elisa Biusing <mayelisabiusing@gmail.com>
2026-04-28 15:55:30 +0800

    meow

--- srcs/stack_init.c
+++ srcs/stack_init.c
@@ -6,8 +6,66 @@
 /*   By: mbiusing <mbiusing@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 15:03:35 by mbiusing          #+#    #+#             */
-/*   Updated: 2026/04/28 15:36:09 by mbiusing         ###   ########.fr       */
+/*   Updated: 2026/04/28 15:53:02 by mbiusing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+void	init_stack(t_stack *stack)
+{
+	stack->head = NULL;
+	stack->tail = NULL;
+	stack->size = 0;
+}
+
+t_node	*new_node(int value)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->num = value;
+	node->next = node;
+	node->prev = node;
+	return (node);
+}
+
+void	add_back(t_stack *stack, int value)
+{
+	t_node	*node;
+
+	node = new_node(value);
+	if (!node)
+		return ;
+	if (stack->size == 0)
+	{
+		stack->head = node;
+		stack->tail = node;
+	}
+	else
+	{
+		node->prev = stack->tail;
+		node->next = stack->head;
+		stack->tail->next = node;
+		stack->head->prev = node;
+		stack->tail = node;
+	}
+	stack->size++;
+}
+
+int	build_stack(t_stack *a, int *values, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		add_back(a, values[i]);
+		i++;
+	}
+	if (a->size != size)
+		return (0);
+	return (1);
+}
--- srcs/validate.c
+++ srcs/validate.c
@@ -6,8 +6,71 @@
 /*   By: mbiusing <mbiusing@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 15:03:30 by mbiusing          #+#    #+#             */
-/*   Updated: 2026/04/28 15:36:07 by mbiusing         ###   ########.fr       */
+/*   Updated: 2026/04/28 15:53:42 by mbiusing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+int	is_valid_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str || !str[0])
+		return (0);
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+long	ft_atol(char *str)
+{
+	long	result;
+	int		sign;
+	int		i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+int	has_duplicate(int *values, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (values[i] == values[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
