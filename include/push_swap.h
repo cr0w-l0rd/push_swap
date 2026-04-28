483280b39016ec3a19e41b0b2c0bc708ba7d2815
May Elisa Biusing <mayelisabiusing@gmail.com>
2026-04-28 15:51:13 +0800

    meow

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mbiusing <mbiusing@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 17:05:24 by mbiusing          #+#    #+#             */
-/*   Updated: 2026/04/28 15:00:15 by mbiusing         ###   ########.fr       */
+/*   Updated: 2026/04/28 15:50:19 by mbiusing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,57 @@ typedef struct s_stack
 	struct s_node	*tail;
 }				t_stack;
 
-t_node	*create_node(int value);
+/* PARSE */
+int		parse_input(int ac, char **av, int **values, int *size);
+int		count_numbers(char **av);
+int		fill_values(char **av, int *values);
+
+/* VALIDATE */
+int		is_valid_number(char *str);
+long	ft_atol(char *str);
+int		has_duplicate(int *values, int size);
+
+/* STACK */
+void	init_stack(t_stack *stack);
+t_node	*new_node(int value);
+void	add_back(t_stack *stack, int value);
+int		build_stack(t_stack *a, int *values, int size);
+int		is_sorted(t_stack *stack);
+
+/* INDEX */
+void	index_values(int *values, int size);
+
+/* SORT */
+void	sort_stack(t_stack *a, t_stack *b);
+void	sort_two(t_stack *a);
+void	sort_three(t_stack *a);
+void	sort_small(t_stack *a, t_stack *b);
+void	radix_sort(t_stack *a, t_stack *b);
+
+/* OPERATIONS */
+
+	/* SWAP */
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
+
+	/* PUSH */
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *a, t_stack *b);
+
+	/* ROTATE */
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+
+	/* RROTATE */
+void	rra(t_stack *a);
+void	rrb(t_stack *b);
+void	rrr(t_stack *a, t_stack *b);
+
+/* FREE & ERROR */
+void	free_stack(t_stack *stack);
+void	free_split(char **split);
+void	error_exit(void);
 
 #endif
--- srcs/parse.c
+++ srcs/parse.c
@@ -6,8 +6,23 @@
 /*   By: mbiusing <mbiusing@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 15:36:14 by mbiusing          #+#    #+#             */
-/*   Updated: 2026/04/28 15:36:16 by mbiusing         ###   ########.fr       */
+/*   Updated: 2026/04/28 15:45:10 by mbiusing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+int	parse_input(int ac, char **av, int **values, int *size)
+{
+	
+}
+
+int	count_numbers(char **av)
+{
+	
+}
+
+int	fill_values(char **av, int *values)
+{
+	
+}
